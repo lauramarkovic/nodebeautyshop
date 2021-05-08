13184a2d4a194b0327f9e782a35d89be27625332
Laura Markovičová <markovicova.laura@gmail.com>
2021-05-08 18:58:35 +0200

    init

---
+++ util/file.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+const deleteFile = (filePath) => {
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      throw (err);
+    }
+  });
+}
+
+exports.deleteFile = deleteFile;
